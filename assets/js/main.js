da1e21c97b769434ad21039a1cd767b3defc74d1
Nazario A. Ayala <nazario@niztech.com>
2015-11-12 10:42:29 -0800

    Separated out data

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,7 @@ window.onload = function(){
   var slide3chart1 = c3.generate({
     bindto: '#slide-3-degrees-needed-graph',
     data: {
-      columns: [
-        ['data1', 10916729, 11011864, 11083431, 11156005, 11244562, 11336323, 11432868, 11525823, 11607133, 11677103, 11732387, 11782761, 11825384, 11860759, 11894741, 11920060],
-        ['data2', 7701256, 7788391, 8018762, 8032324, 8187211, 8341821, 8484984, 8626679, 8760757, 8887231, 9003317, 9116301, 9223876, 9326289, 9428045, 9523307],
-        ['gap', 3215473, 3223473, 3064669, 3123681, 3057351, 2994502, 2947884, 2899144, 2846376, 2789872, 2729070, 2666460, 2601508, 2534470, 2466696, 2396753]
-      ],
+      columns: dataSlide3chart1,
       type: 'area',
       groups: [
         ['data1', 'data2']
@@ -27,7 +23,7 @@ window.onload = function(){
       hide: true,
       hide: ['gap'],
       onmouseover: function(d) {
-        
+
         if(d.id === 'gap'){
           var gap_array= d.value.toString().split("");
           var counter_squares = $('#degree-deficit-counter .counter').children('span');
@@ -64,9 +60,7 @@ window.onload = function(){
   var slide5chart1 = c3.generate({
     bindto: '#slide-5-top-ten-majors-graph',
     data: {
-      columns: [
-        ['data1', 10916729, 11011864, 11083431, 11156005, 11244562, 11336323, 11432868, 11525823, 11607133, 11677103, 11732387, 11782761, 11825384, 11860759, 11894741, 11920060],
-      ],
+      columns: dataSlide5chart1,
       type: 'pie',
       groups: [
         ['data1', 'data2']

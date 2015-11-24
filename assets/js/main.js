74a67efafe76723e43c89b3d857fd89392242b58
Nazario A. Ayala <nazario@niztech.com>
2015-11-23 18:13:16 -0800

    Added basic accordian functionality to recomendations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 
-window.onload = function(){
 
-}
+$(document).ready(function(){
+
+  $('#container-recomendations dt').on('click', function(d){
+
+    if($(this).hasClass('inactive')) {
+      $('#container-recomendations dt').addClass('inactive');
+      $('#container-recomendations dd').addClass('hidden');
+      $(this).removeClass('inactive');
+      $(this).next('dd').removeClass('hidden');
+    }
+    else {
+      $(this).addClass('inactive');
+      $(this).next('dd').addClass('hidden');
+    }
+  });
+
+});

e4724ddcc98f0ef4e4279a6ac0d7def3a509a97d
Nazario A. Ayala <nazario@niztech.com>
2015-11-26 07:24:08 -0800

    Attempt at adding the tooltip

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -104,6 +104,11 @@ window.onload = function(){
         }
       }
     },
+    tooltip: {
+      contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+        return '<div class="tooltip"><div class="major">The Stats <span>' + d[0].name + '</span></div><hr><ul><li class="percent">Percent of all BAs in 2013<br /><span>' + percentify(d[0].ratio) + '</span></li><li class="rank">Rank in 2004<br /><span>' + 10 + '</span></li><li class="top-providers">Top Providers<br /><span>' + d[0].TopProviders + '</span></li></ul></div>';
+      }
+    }
   });
 
   // slide

a950f0337615010f6f992a889a95e5a1ab3141c0
Nazario A. Ayala <nazario@niztech.com>
2015-12-04 15:51:18 -0800

    Claned up data indentation for redability

--- assets/js/data.js
+++ assets/js/data.js
@@ -205,55 +205,64 @@ var slide_3_1_0_data = [
 ];
 
 var slide_3_3_0_data = [
-  {"Ethicity": "American Indian/Alaska Native",
+  {
+    "Ethicity": "American Indian/Alaska Native",
     "UC": 188,
     "CSU": 414,
     "NonProfit": 209,
     "ForProfit": 81
   },
-  {"Ethicity": "Asian",
+  {
+    "Ethicity": "Asian",
     "UC": 17374,
     "CSU": 13413,
     "NonProfit": 4757,
     "ForProfit": 1477
   },
-  {"Ethicity": "Black/African American",
+  {
+    "Ethicity": "Black/African American",
     "UC": 1397,
     "CSU": 3602,
     "NonProfit": 1754,
     "ForProfit": 1211
   },
-  {"Ethicity": "Hispanic/Latino",
+  {
+    "Ethicity": "Hispanic/Latino",
     "UC": 8488,
     "CSU": 21686,
     "NonProfit": 6694,
     "ForProfit": 3259
   },
-  {"Ethicity": "Native Hawaiian or Other Pacific Islander",
+  {
+    "Ethicity": "Native Hawaiian or Other Pacific Islander",
     "UC": 149,
     "CSU": 597,
     "NonProfit": 234,
     "ForProfit": 174
   },
-  {"Ethicity": "White",
+  {
+    "Ethicity": "White",
     "UC": 15798,
     "CSU": 28990,
     "NonProfit": 17046,
     "ForProfit": 3279
   },
-  {"Ethicity": "Two or more races",
+  {
+    "Ethicity": "Two or more races",
     "UC": 950,
     "CSU": 2506,
     "NonProfit": 1281,
     "ForProfit": 184
   },
-  {"Ethicity": "Unknown",
+  {
+    "Ethicity": "Unknown",
     "UC": 2518,
     "CSU": 7576,
     "NonProfit": 2330,
     "ForProfit": 3294
   },
-  {"Ethicity": "Nonresident alien",
+  {
+    "Ethicity": "Nonresident alien",
     "UC": 2084,
     "CSU": 3019,
     "NonProfit": 2074,
@@ -263,162 +272,162 @@ var slide_3_3_0_data = [
 
 
 var dataYellowSlide = {
- "Business, Management, Marketing & Related Support Services": {
-  "Overall rank": 1,
-  "American Indian/Alaska Native": 1,
-  "Asian": 1,
-  "Black/African American": 1,
-  "Hispanic/Latino": 1,
-  "Native Hawaiian or Other Pacific Islander": 1,
-  "White": 1,
-  "Two or more races": 2,
-  "Unknown": 1,
-  "Nonresident alien": 1
- },
- "Social Sciences": {
-  "Overall rank": 2,
-  "American Indian/Alaska Native": 2,
-  "Asian": 3,
-  "Black/African American": 2,
-  "Hispanic/Latino": 2,
-  "Native Hawaiian or Other Pacific Islander": 4,
-  "White": 2,
-  "Two or more races": 1,
-  "Unknown": 2,
-  "Nonresident alien": 2
- },
- "Psychology": {
-  "Overall rank": 3,
-  "American Indian/Alaska Native": 4,
-  "Asian": 6,
-  "Black/African American": 3,
-  "Hispanic/Latino": 3,
-  "Native Hawaiian or Other Pacific Islander": 6,
-  "White": 4,
-  "Two or more races": 3,
-  "Unknown": 5,
-  "Nonresident alien": 5
- },
- "Visual & Performing Arts": {
-  "Overall rank": 4,
-  "American Indian/Alaska Native": 3,
-  "Asian": 7,
-  "Black/African American": 5,
-  "Hispanic/Latino": 4,
-  "Native Hawaiian or Other Pacific Islander": 7,
-  "White": 3,
-  "Two or more races": 4,
-  "Unknown": 3,
-  "Nonresident alien": 3
- },
- "Biological & Biomedical Sciences": {
-  "Overall rank": 5,
-  "American Indian/Alaska Native": 6,
-  "Asian": 2,
-  "Black/African American": 8,
-  "Hispanic/Latino": 9,
-  "Native Hawaiian or Other Pacific Islander": 5,
-  "White": 6,
-  "Two or more races": 7,
-  "Unknown": 6,
-  "Nonresident alien": 6
- },
- "Health Professions & Related Programs": {
-  "Overall rank": 6,
-  "American Indian/Alaska Native": 5,
-  "Asian": 4,
-  "Black/African American": 4,
-  "Hispanic/Latino": 5,
-  "Native Hawaiian or Other Pacific Islander": 2,
-  "White": 5,
-  "Two or more races": 5,
-  "Unknown": 4,
-  "Nonresident alien": 10
- },
- "Engineering": {
-  "Overall rank": 7,
-  "American Indian/Alaska Native": 9,
-  "Asian": 5,
-  "Black/African American": 15,
-  "Hispanic/Latino": 11,
-  "Native Hawaiian or Other Pacific Islander": 3,
-  "White": 8,
-  "Two or more races": 9,
-  "Unknown": 7,
-  "Nonresident alien": 4
- },
- "Communication, Journalism & Related Programs": {
-  "Overall rank": 8,
-  "American Indian/Alaska Native": 8,
-  "Asian": 8,
-  "Black/African American": 6,
-  "Hispanic/Latino": 8,
-  "Native Hawaiian or Other Pacific Islander": 9,
-  "White": 7,
-  "Two or more races": 6,
-  "Unknown": 9,
-  "Nonresident alien": 7
- },
- "English Language & Literature/Letters": {
-  "Overall rank": 9,
-  "American Indian/Alaska Native": 10,
-  "Asian": 12,
-  "Black/African American": 9,
-  "Hispanic/Latino": 10,
-  "Native Hawaiian or Other Pacific Islander": 12,
-  "White": 9,
-  "Two or more races": 8,
-  "Unknown": 10,
-  "Nonresident alien": 15
- },
- "Liberal Arts and Sciences, General Studies & Humanities": {
-  "Overall rank": 10,
-  "American Indian/Alaska Native": 7,
-  "Asian": 15,
-  "Black/African American": 12,
-  "Hispanic/Latino": 7,
-  "Native Hawaiian or Other Pacific Islander": 13,
-  "White": 10,
-  "Two or more races": 10,
-  "Unknown": 8,
-  "Nonresident alien": 13
- },
- "Homeland Security, Law Enforcement, Firefighting & Related Protective Service": {
-  "Overall rank": 11,
-  "American Indian/Alaska Native": 13,
-  "Asian": 18,
-  "Black/African American": 7,
-  "Hispanic/Latino": 6,
-  "Native Hawaiian or Other Pacific Islander": 9,
-  "White": 17,
-  "Two or more races": 14,
-  "Unknown": 11,
-  "Nonresident alien": 27
- },
- "Computer & Information Sciences ": {
-  "Overall rank": 12,
-  "American Indian/Alaska Native": 17,
-  "Asian": 9,
-  "Black/African American": 11,
-  "Hispanic/Latino": 19,
-  "Native Hawaiian or Other Pacific Islander": 8,
-  "White": 14,
-  "Two or more races": 15,
-  "Unknown": 12,
-  "Nonresident alien": 9
- },
- "Parks, Recreation, Leisure & Fitness Studies": {
-  "Overall rank": 13,
-  "American Indian/Alaska Native": 12,
-  "Asian": 14,
-  "Black/African American": 16,
-  "Hispanic/Latino": 15,
-  "Native Hawaiian or Other Pacific Islander": 11,
-  "White": 11,
-  "Two or more races": 11,
-  "Unknown": 13,
-  "Nonresident alien": 17
- }
+  "Business, Management, Marketing & Related Support Services": {
+    "Overall rank": 1,
+    "American Indian/Alaska Native": 1,
+    "Asian": 1,
+    "Black/African American": 1,
+    "Hispanic/Latino": 1,
+    "Native Hawaiian or Other Pacific Islander": 1,
+    "White": 1,
+    "Two or more races": 2,
+    "Unknown": 1,
+    "Nonresident alien": 1
+  },
+  "Social Sciences": {
+    "Overall rank": 2,
+    "American Indian/Alaska Native": 2,
+    "Asian": 3,
+    "Black/African American": 2,
+    "Hispanic/Latino": 2,
+    "Native Hawaiian or Other Pacific Islander": 4,
+    "White": 2,
+    "Two or more races": 1,
+    "Unknown": 2,
+    "Nonresident alien": 2
+  },
+  "Psychology": {
+    "Overall rank": 3,
+    "American Indian/Alaska Native": 4,
+    "Asian": 6,
+    "Black/African American": 3,
+    "Hispanic/Latino": 3,
+    "Native Hawaiian or Other Pacific Islander": 6,
+    "White": 4,
+    "Two or more races": 3,
+    "Unknown": 5,
+    "Nonresident alien": 5
+  },
+  "Visual & Performing Arts": {
+    "Overall rank": 4,
+    "American Indian/Alaska Native": 3,
+    "Asian": 7,
+    "Black/African American": 5,
+    "Hispanic/Latino": 4,
+    "Native Hawaiian or Other Pacific Islander": 7,
+    "White": 3,
+    "Two or more races": 4,
+    "Unknown": 3,
+    "Nonresident alien": 3
+  },
+  "Biological & Biomedical Sciences": {
+    "Overall rank": 5,
+    "American Indian/Alaska Native": 6,
+    "Asian": 2,
+    "Black/African American": 8,
+    "Hispanic/Latino": 9,
+    "Native Hawaiian or Other Pacific Islander": 5,
+    "White": 6,
+    "Two or more races": 7,
+    "Unknown": 6,
+    "Nonresident alien": 6
+  },
+  "Health Professions & Related Programs": {
+    "Overall rank": 6,
+    "American Indian/Alaska Native": 5,
+    "Asian": 4,
+    "Black/African American": 4,
+    "Hispanic/Latino": 5,
+    "Native Hawaiian or Other Pacific Islander": 2,
+    "White": 5,
+    "Two or more races": 5,
+    "Unknown": 4,
+    "Nonresident alien": 10
+  },
+  "Engineering": {
+    "Overall rank": 7,
+    "American Indian/Alaska Native": 9,
+    "Asian": 5,
+    "Black/African American": 15,
+    "Hispanic/Latino": 11,
+    "Native Hawaiian or Other Pacific Islander": 3,
+    "White": 8,
+    "Two or more races": 9,
+    "Unknown": 7,
+    "Nonresident alien": 4
+  },
+  "Communication, Journalism & Related Programs": {
+    "Overall rank": 8,
+    "American Indian/Alaska Native": 8,
+    "Asian": 8,
+    "Black/African American": 6,
+    "Hispanic/Latino": 8,
+    "Native Hawaiian or Other Pacific Islander": 9,
+    "White": 7,
+    "Two or more races": 6,
+    "Unknown": 9,
+    "Nonresident alien": 7
+  },
+  "English Language & Literature/Letters": {
+    "Overall rank": 9,
+    "American Indian/Alaska Native": 10,
+    "Asian": 12,
+    "Black/African American": 9,
+    "Hispanic/Latino": 10,
+    "Native Hawaiian or Other Pacific Islander": 12,
+    "White": 9,
+    "Two or more races": 8,
+    "Unknown": 10,
+    "Nonresident alien": 15
+  },
+  "Liberal Arts and Sciences, General Studies & Humanities": {
+    "Overall rank": 10,
+    "American Indian/Alaska Native": 7,
+    "Asian": 15,
+    "Black/African American": 12,
+    "Hispanic/Latino": 7,
+    "Native Hawaiian or Other Pacific Islander": 13,
+    "White": 10,
+    "Two or more races": 10,
+    "Unknown": 8,
+    "Nonresident alien": 13
+  },
+  "Homeland Security, Law Enforcement, Firefighting & Related Protective Service": {
+    "Overall rank": 11,
+    "American Indian/Alaska Native": 13,
+    "Asian": 18,
+    "Black/African American": 7,
+    "Hispanic/Latino": 6,
+    "Native Hawaiian or Other Pacific Islander": 9,
+    "White": 17,
+    "Two or more races": 14,
+    "Unknown": 11,
+    "Nonresident alien": 27
+  },
+  "Computer & Information Sciences ": {
+    "Overall rank": 12,
+    "American Indian/Alaska Native": 17,
+    "Asian": 9,
+    "Black/African American": 11,
+    "Hispanic/Latino": 19,
+    "Native Hawaiian or Other Pacific Islander": 8,
+    "White": 14,
+    "Two or more races": 15,
+    "Unknown": 12,
+    "Nonresident alien": 9
+  },
+  "Parks, Recreation, Leisure & Fitness Studies": {
+    "Overall rank": 13,
+    "American Indian/Alaska Native": 12,
+    "Asian": 14,
+    "Black/African American": 16,
+    "Hispanic/Latino": 15,
+    "Native Hawaiian or Other Pacific Islander": 11,
+    "White": 11,
+    "Two or more races": 11,
+    "Unknown": 13,
+    "Nonresident alien": 17
+  }
 }
 
 var slide_3_4_0_data = [

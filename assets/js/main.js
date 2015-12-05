346f2ee8c4d1d19fbb3015538b0a6a516b2bba9b
Nazario A. Ayala <nazario@niztech.com>
2015-12-04 21:37:31 -0800

    Added smotth scrolling for a href=# elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,4 +23,14 @@ $(document).ready(function(){
     }
   });
 
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $( $(this).attr('href') );
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+
 });

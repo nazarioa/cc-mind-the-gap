054d61136d5fcdab295e5ddda7ed1f9698af406c
Nazario A. Ayala <nazario@niztech.com>
2015-12-07 19:51:19 -0800

    refactored and leveraged code for the-stats side data

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,46 +14,47 @@ $(document).ready(function () {
     }
   });
 
-  $('#slide-3-3 dt').on('click', function () {
+  $('.the-stats dt').on('click', function () {
+    var parent_id = $(this).parents('section.the-stats').attr("id");
 
     if($(this).hasClass('inactive')) {
-      $('#slide-3-3 dt').addClass('inactive');
-      $('#slide-3-3 dd').addClass('hidden');
+      $('#' + parent_id + ' dt').addClass('inactive');
+      $('#' + parent_id + ' dd').addClass('hidden');
       $(this).removeClass('inactive');
       $(this).next('dd').removeClass('hidden');
     }
   });
 
-  $('#slide-3-3 .back').on('click', function () {
-    var leftPanelWidth = $('#slide-3-3 .left-panel').width();
+  $('.the-stats .back').on('click', function () {
+    var parent_id = $(this).parents('section.the-stats').attr("id");
+
+    var leftPanelWidth = $('#' + parent_id + ' .left-panel').width();
 
     // If the info panel is not .hidden, slide it out and present data panel
-    if($('#slide-3-3 .left-panel.data').hasClass('hidden') === false ) {
+    if($('#' + parent_id + ' .left-panel.data').hasClass('hidden') === false ) {
 
       //start info slide out animation
-      $('#slide-3-3 .left-panel.data').animate(
+      $('#' + parent_id + ' .left-panel.data').animate(
         {left: -leftPanelWidth},
         500,
         "swing",
         function () {
           // swap out the info div with the data div
-          $('#slide-3-3 .left-panel.info').removeClass('hidden');
-          $('#slide-3-3 .left-panel.data').addClass('hidden');
+          $('#' + parent_id + ' .left-panel.info').removeClass('hidden');
+          $('#' + parent_id + ' .left-panel.data').addClass('hidden');
         }
       );
       //end info slide out animation
 
       //start data slide in animation
-      $('#slide-3-3 .left-panel.info').css('left', -leftPanelWidth);
-      $('#slide-3-3 .left-panel.info').animate(
+      $('#' + parent_id + ' .left-panel.info').css('left', -leftPanelWidth);
+      $('#' + parent_id + ' .left-panel.info').animate(
         {left: 0},
         1500,
         "swing"
       );
       //end data slide in animation
-
     }
-
   });
 
   $('a[href^="#"]').on('click', function (event) {
